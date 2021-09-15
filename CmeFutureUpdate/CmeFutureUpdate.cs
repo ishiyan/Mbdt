ef78db6a4663ec104a51ffbc87cfd20c79644a32
Ivan Shiyan <ivan.shiyan@gmail.com>
2021-09-15 02:51:02 +0200

    cme update

--- CmeFutureUpdate/CmeFutureUpdate.cs
+++ CmeFutureUpdate/CmeFutureUpdate.cs
@@ -169,7 +169,8 @@ namespace CmeFutureUpdate
                    new SeriesEntry { Code="ESZ21", Month = "DEC 2021", MonthShortcut="DEC-21", FirstTradeDate=new DateTime(2020,  9, 18), LastTradeDate=new DateTime(2021, 12, 17), SettlementDate=new DateTime(2021, 12, 17), RolloverDate=new DateTime(2021, 12,  9)},
                    new SeriesEntry { Code="ESH22", Month = "MAR 2022", MonthShortcut="MAR-22", FirstTradeDate=new DateTime(2020, 12, 18), LastTradeDate=new DateTime(2022,  3, 18), SettlementDate=new DateTime(2022,  3, 18), RolloverDate=new DateTime(2022,  3,  10)},
                    new SeriesEntry { Code="ESM22", Month = "JUN 2022", MonthShortcut="JUN-22", FirstTradeDate=new DateTime(2021,  3, 19), LastTradeDate=new DateTime(2022,  6, 17), SettlementDate=new DateTime(2022,  3, 18), RolloverDate=new DateTime(2022,  6,  9)},
-                   new SeriesEntry { Code="ESU22", Month = "SEP 2022", MonthShortcut="SEP-22", FirstTradeDate=new DateTime(2021,  6,  7), LastTradeDate=new DateTime(2022,  9, 16), SettlementDate=new DateTime(2022,  9, 16), RolloverDate=new DateTime(2022,  9,  8)}//,
+                   new SeriesEntry { Code="ESU22", Month = "SEP 2022", MonthShortcut="SEP-22", FirstTradeDate=new DateTime(2021,  6,  7), LastTradeDate=new DateTime(2022,  9, 16), SettlementDate=new DateTime(2022,  9, 16), RolloverDate=new DateTime(2022,  9,  8)},
+                   new SeriesEntry { Code="ESZ22", Month = "DEC 2022", MonthShortcut="DEC-22", FirstTradeDate=new DateTime(2021,  9,  13), LastTradeDate=new DateTime(2022,  12, 16), SettlementDate=new DateTime(2022,  12, 16), RolloverDate=new DateTime(2022,  12,  8)}//,
                 },
                TimeSlotList = new List<TimeSlot>
                {
